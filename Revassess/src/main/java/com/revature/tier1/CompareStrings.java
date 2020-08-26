245743bf789f905f8bb48e5f8f822abbdad630a9
Jasmine Edison <bigbluedanube@gmail.com>
2020-08-26 14:53:57 -0400

    completed CompareStrings part of tier1.

--- Revassess/src/main/java/com/revature/tier1/CompareStrings.java
+++ Revassess/src/main/java/com/revature/tier1/CompareStrings.java
@@ -3,6 +3,13 @@ package com.revature.tier1;
 public class CompareStrings {
 
     public static boolean compareStrings(String s1, String s2){
-        return false;
+		return false;
     }
+
+	public String s1;
+	public String s2;
+    
+	public CompareStrings() {
+		super();
+	}
 }
--- Revassess/src/main/java/com/revature/tier1/User.java
+++ Revassess/src/main/java/com/revature/tier1/User.java
@@ -1,4 +1,10 @@
 package com.revature.tier1;
 
 public class User {
+	private int id;
+	private String firstName;
+	private String lastName;
+	private String userName;
+	private String password;
+	private String role;
 }
