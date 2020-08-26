b9f4a443fb96b2b06d79f4dd49538db41cfa73b0
Jasmine Edison <bigbluedanube@gmail.com>
2020-08-26 16:25:49 -0400

    finished the NumberSumLength assignment.

--- Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
+++ Revassess/src/main/java/com/revature/tier1/NumberSumLength.java
@@ -3,6 +3,25 @@ package com.revature.tier1;
 public class NumberSumLength {
 
 	public static boolean checkNumberPowerLength(long num) {
+		Long myLong = num;											// Auto-boxed to give me more functionality.
+		int length = String.valueOf(myLong).length();				// this gives us the number of digits.
+		
+		for(int i = 0; i < length; i++) {
+			double exponentLong = Math.pow(myLong, i);								// raises each digit to the power of the length.
+			System.out.println("Each digit raised to the power of the length is: ");
+			
+			long powerSum = 0;
+			powerSum += i;
+			
+			System.out.println(i);
+			System.out.println(myLong);
+			
+			System.out.println("Sum of the powers: " + powerSum);
+			
+			boolean myAnswer = myLong.equals(powerSum);
+			System.out.println("The boolean value for this question is " + myAnswer);
+		}
+		
 		return false;
 	}
 }
